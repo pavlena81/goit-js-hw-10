e61187a5abd2400bb5a0b790f420ef77133cbeb6
Olena Nesteruk <pavlena81@gmail.com>
2022-08-23 01:27:01 +0300

    adds part request

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -35,16 +35,28 @@ const fetchCountriesById = document.querySelector('#search-box');
 
 const  countryList = document.querySelector('.country-list');
 
-fetchCountriesById.addEventListener("click", () => {
-  fetchCountries()
-    .then((users) => renderUserList(users))
-    .catch((error) => console.log(error));
-});
-
-function fetchCountries(e) {
+fetchCountriesById.addEventListener("click", onSearch);
+  
+  
+function onSearch(e) {
   e.preventDefault();
+  
+   fetchCountriesById.textContent = e.currentTarget.value;
+
+  fetchCountries()
+    .then(renderCountryList)
+    .catch((error) => console.log(error))
+    .finally(()=> reset())
+};
+
+// const SEARCH_PARAMS = 'name,capital,population,flags,languages';
+
+
+function fetchCountries() { 
+
+  
   return fetch(
-    "https://restcountries.com/#api-endpoints-v3-name"
+    "https://restcountries.com/v2/all?fields=name,capital,population,flags,languages"
   ).then((response) => {
     if (!response.ok) {
       throw new Error(response.status);
@@ -72,16 +84,7 @@ function renderCountryList(users) {
 
 export default fetchCountries;
 //=========================================
-// const searchParams = new URLSearchParams({
-//   name.official:полное имя страны,
-// capital: столица,
-// population: население,
-// flags.svg:ссылка на изображение флага,
-// languages:[languages],
-  
-//   _limit: 5,
-//   _sort: "name",
-// });
+
 
 // console.log(searchParams.toString()); // "_limit=5&_sort=name"
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './css/styles.css';
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import _ from 'lodash.debounce';
+
+// var debounce = require('lodash.debounce');
+
 import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
